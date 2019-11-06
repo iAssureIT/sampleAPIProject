c94fa804166b49d04f90a74ca7996c151dfbd221
iAssureIT <iAssureIT@gmail.com>
2019-11-06 21:32:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@
 		}
 		next();
 	});
-	console.log("sampleAPIProject 3");
+	console.log("sampleAPIProject 4");
 	app.use("/", (req,res,next)=>{
 		res.status(200).json("Sample API local commit");
 	});
